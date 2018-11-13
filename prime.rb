75c924d86017d55234ffa31c1e2d341a1250b82f
Kiley Bobbitt <jkileybob@aol.com>
2018-11-13 21:02:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,10 @@ def prime?(integer)
   if integer <= 1
     return false
   end
-  for i in 2..(int - 1)
-  if (int % i) == 0
+  for i in 2..(integer - 1)
+  if (integer % i) == 0
     return false
   end
 end
 return true
-end 
+end
